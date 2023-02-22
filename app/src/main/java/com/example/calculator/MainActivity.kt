fe39a3fdbf12ce1d4a79b1548994893986d5f06c
cipher <pratyushchauhan62@gmail.com>
2023-02-22 13:00:23 +0530

    initial commit

---
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.calculator
+
+import android.os.Bundle
+import android.view.View
+
+
+private var _binding: ResultProfileBinding? = null
+// This property is only valid between onCreateView and
+// onDestroyView.
+private val binding get() = _binding!!
+
+override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+): View? {
+    _binding = ResultProfileBinding.inflate(inflater, container, false)
+    val view = binding.root
+    return view
+}
+
+override fun onDestroyView() {
+    super.onDestroyView()
+    _binding = null
+}
